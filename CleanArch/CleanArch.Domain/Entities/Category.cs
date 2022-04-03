5af6555ccf299614742746909ffc07deaad879df
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-03 18:57:17 -0300

    enriquecendo modelos de dominio

---
+++ CleanArch/CleanArch.Domain/Entities/Category.cs
@@ -0,0 +1,35 @@
+﻿using CleanArch.Domain.Validation;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "ID is invalid.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; private set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3, "Name is too short. Minimum required 3.");
+
+            Name = name;
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+        public int CategoryId { get; private set; }
+        public Category Category { get; private set; }
+    }
+}
