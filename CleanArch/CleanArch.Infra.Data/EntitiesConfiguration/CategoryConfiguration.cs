0eeef49361cef0fc4fe55e6949afc728aa5e6462
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-01 21:54:17 -0300

    adding product and category db configuration

---
+++ CleanArch/CleanArch.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Computador"),
+                new Category(2, "Acessórios"),
+                new Category(3, "Livros")
+            );
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Price).HasPrecision(10, 2);
+            builder.HasOne(c => c.Category).WithMany(c => c.Products).HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
