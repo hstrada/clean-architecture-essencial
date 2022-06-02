b821f8738fd83b521be372251f2eea5d46d269e2
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-01 21:48:29 -0300

    creating application dbcontext

---
+++ CleanArch/CleanArch.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { }
+
+        // nome da tabela
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationException).Assembly);
+        }
+    }
+}
