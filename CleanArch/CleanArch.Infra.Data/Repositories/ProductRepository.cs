f203204f4556cb235d120150dd36ea8a9fa2d63f
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-02 21:45:48 -0300

    create product repository implementation

---
+++ CleanArch/CleanArch.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,58 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetByIdAsync(int? Id)
+        {
+            return await _context.Products.FindAsync(Id);
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int? Id)
+        {
+            return await _context.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == Id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
