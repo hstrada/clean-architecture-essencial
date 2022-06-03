8a32cbbc19129425f7623db45716f943a3faff8e
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-02 21:33:52 -0300

    creating category repository

---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetById(int? Id)
+        {
+            return await _context.Categories.FindAsync(Id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+    }
+}
