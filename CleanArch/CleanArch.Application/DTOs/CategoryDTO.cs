8272aa01eb7621d8f3d6a10b33528cbb90156a80
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-03 20:39:27 -0300

    creating application

---
+++ CleanArch/CleanArch.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArch.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+CleanArch.Infra.Data
+
+-- criar a migração
+> add-migration Initial
+
+-- criar no banco de dados
+> update-database
+
+-- adicionar seed
+> add-migration SeedProducts
