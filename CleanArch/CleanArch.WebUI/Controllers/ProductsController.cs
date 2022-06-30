1c6700e027df115b6df9404c824695719ad56425
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-29 23:05:00 -0300

    creating products cqrs

---
+++ CleanArch/CleanArch.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArch.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productAppService)
+        {
+            _productService = productAppService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProducts();
+            return View(products);
+        }
+    }
+}
