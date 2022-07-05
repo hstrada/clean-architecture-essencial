39547e07643f133333039ff1dd6a33039eea3c48
hstrada <6079947+hstrada@users.noreply.github.com>
2022-07-04 21:08:32 -0300

    creating identity context

--- CleanArch/CleanArch.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArch/CleanArch.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,14 +1,12 @@
 ﻿using CleanArch.Domain.Entities;
+using CleanArch.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArch.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         { }
--- README.md
+++ README.md
@@ -3,8 +3,11 @@ CleanArch.Infra.Data
 -- criar a migração
 > add-migration Initial
 
--- criar no banco de dados
+-- aplicar o script de migração
 > update-database
 
 -- adicionar seed
 > add-migration SeedProducts
+
+-- criar a migração para o contexto do identity
+> add-migration CreateIdentityTables
